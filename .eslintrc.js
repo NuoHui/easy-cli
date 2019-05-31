c989b876ac2c1999d39ae6a0e2936e646aae4543
xuyizong <xuyz@dachentech.com.cn>
2019-05-31 18:08:10 +0800

    chore: 关闭eslint规则no-useless-return

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es6': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
   },
-  'extends': ['standard', 'plugin:prettier/recommended'],
-  'globals': {
-  },
-  'parserOptions': {
+  extends: ['standard', 'plugin:prettier/recommended'],
+  globals: {},
+  parserOptions: {
     ecmaVersion: 2018
   },
-  "plugins": ["prettier"],
-  'rules': {
+  plugins: ['prettier'],
+  rules: {
+    'no-useless-return': 0
   }
-}
+};
