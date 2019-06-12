84dfcd44f8a041d2f7799097ecf455040266c980
xuyizong <xuyz@dachentech.com.cn>
2019-06-12 15:30:34 +0800

    fix: 版本更新,commitlint错误

--- scripts/release.js
+++ scripts/release.js
@@ -34,7 +34,7 @@ const release = async () => {
     }
   ]);
   if (yes) {
-    execSync(`npm version ${release} -m "chore: Update to %s"`, {
+    execSync(`npm version ${release} -m "chore: update to %s"`, {
       stdio: 'inherit'
     });
   }
